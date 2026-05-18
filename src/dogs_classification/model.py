8a6c49de8ed84c7e992510443b92e13fdd3301b6
soenia <sophie.hopp@campus.lmu.de>
2026-05-18 15:31:55 +0200

    add train.py

--- src/dogs_classification/model.py
+++ src/dogs_classification/model.py
@@ -1,14 +1,15 @@
 from transformers import ViTForImageClassification
 
 
-def main():
-    ViTForImageClassification.from_pretrained(
-        "google/vit-base-patch16-224", num_labels=120, ignore_mismatched_sizes=True
-    )
+class DogModel:
+    def __init__(self):
+        self.model = ViTForImageClassification.from_pretrained(
+            "google/vit-base-patch16-224", num_labels=120, ignore_mismatched_sizes=True
+        )
 
 
 # num_labels=120 due to 120 dog breeds
 
 
 if __name__ == "__main__":
-    main()
+    model = DogModel()
