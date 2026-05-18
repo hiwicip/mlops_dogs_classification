e6e4720b146e3d667043f2b63c3885fda297473c
soenia <sophie.hopp@campus.lmu.de>
2026-05-18 15:31:55 +0200

    add train.py

--- src/dogs_classification/train.py
+++ src/dogs_classification/train.py
@@ -1,11 +1,50 @@
-from dogs_classification.data import MyDataset
-from dogs_classification.model import Model
+import torch
+
+from dogs_classification.data import DogDataset
+from dogs_classification.model import DogModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def train():
-    dataset = MyDataset("data/raw")  # noqa: F841
-    model = Model()  # noqa: F841
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10):
+    train_dataset = DogDataset("data/processed/metadata.csv", "train")  # noqa: F841
+    test_dataset = DogDataset("data/processed/metadata.csv", "test")  # noqa: F841
+    model = DogModel()  # noqa: F841
+    model.to(DEVICE)
     # add rest of your training code here. Remove noqa when model and dataset variables are used.
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size)
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    statistics = {"train_loss": [], "train_accuracy": []}
+    for epoch in range(epochs):
+        model.train()
+        for i, (img, target) in enumerate(train_dataloader):
+            # Move images and targets to the appropriate device
+            img, target = img.to(DEVICE), target.to(DEVICE)
+
+            optimizer.zero_grad()  # Reset gradients
+
+            # Forward pass: compute predictions and loss
+            y_pred = model(img)
+            loss = loss_fn(y_pred, target)
+
+            # Backward pass: compute gradients
+            loss.backward()
+
+            optimizer.step()  # Update model parameters
+
+            statistics["train_loss"].append(loss.item())
+
+            # Compute and record the training accuracy
+            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+            statistics["train_accuracy"].append(accuracy)
+
+            # Print progress every 100 iterations
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
 
 if __name__ == "__main__":
