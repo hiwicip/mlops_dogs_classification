1f7d7d95d7c9c54b725954decc30595fb7902dee
soenia <sophie.hopp@campus.lmu.de>
2026-05-27 12:25:41 +0200

    add dummy data generation for data tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,22 @@
+import pandas as pd
 from dogs_classification.data import DogDataset
 from torch.utils.data import Dataset
 
 
-def test_dog_dataset():
+def test_dog_dataset(tmp_path):
     """Test the DogDataset class."""
-    dataset = DogDataset("data/processed/metadata.csv", split="train")
+
+    metadata = pd.DataFrame(
+        {
+            "split": ["train"],
+            "image_path": ["path/to/image1.pt"],
+            "label": [0],
+            "breed": ["breed1"],
+        }
+    )
+
+    metadata_path = tmp_path / "metadata.csv"
+    metadata.to_csv(metadata_path, index=False)
+
+    dataset = DogDataset(metadata_path, split="train")
     assert isinstance(dataset, Dataset)
