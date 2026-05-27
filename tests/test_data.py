e27a6a05229379d821de1d7b15eeae9799af49c5
soenia <sophie.hopp@campus.lmu.de>
2026-05-27 11:01:17 +0200

    fix test_dog_dataset

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,5 +4,5 @@ from torch.utils.data import Dataset
 
 def test_dog_dataset():
     """Test the DogDataset class."""
-    dataset = DogDataset("data/raw")
+    dataset = DogDataset("data/processed/metadata.csv", split="train")
     assert isinstance(dataset, Dataset)
